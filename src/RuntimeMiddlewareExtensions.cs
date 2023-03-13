4b0676c8b0567080f7cfec0bf3eee13c0e9ebb5e
deximy <deximy@gmail.com>
2023-03-14 01:10:12 +0800

    feat: add a middleware that allows to add middlewares during runtime

---
+++ src/RuntimeMiddlewareExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Extensions.Middlewares.Plugin
+{
+    public static class RuntimeMiddlewareExtensions
+    {
+        public static IServiceCollection AddRuntimeMiddleware(this IServiceCollection services, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        {
+            services.Add(new ServiceDescriptor(typeof(RuntimeMiddlewareService), typeof(RuntimeMiddlewareService), lifetime));
+            return services;
+        }
+
+        public static IApplicationBuilder UseRuntimeMiddleware(this IApplicationBuilder app, Action<IApplicationBuilder>? default_action = null)
+        {
+            var service = app.ApplicationServices.GetRequiredService<RuntimeMiddlewareService>();
+            service.Use(app);
+            if (default_action != null)
+            {
+                service.Configure(default_action);
+            }
+            return app;
+        }
+    }
+}
---
+++ src/RuntimeMiddlewareService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Extensions.Middlewares.Plugin
+{
+    internal class RuntimeMiddlewareService
+    {
+        private IApplicationBuilder? app_builder_;
+        private Func<RequestDelegate, RequestDelegate>? middleware_;
+
+        internal void Use(IApplicationBuilder app)
+        {
+            app_builder_ = app.Use(next => context => middleware_ == null ? next(context) : middleware_(next)(context));
+        }
+
+        public void Configure(Action<IApplicationBuilder> action)
+        {
+            if (app_builder_== null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var app = app_builder_.New();
+            action(app);
+            middleware_ = next => app.Use(_ => next).Build();
+        }
+    }
+}
