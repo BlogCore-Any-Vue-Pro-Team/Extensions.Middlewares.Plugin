c103a585d38763b0d6a7c64d644e5f2ac56ff90a
deximy <deximy@gmail.com>
2023-03-14 02:07:05 +0800

    feat: add an example of a custom page in plugins

---
+++ example/Plugin/Class.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Plugin
+{
+    public class Class
+    {
+        public void RegisterMiddleware(IApplicationBuilder app)
+        {
+            app.Map(
+                "/plugin-page",
+                (app_builder) => {
+                    app_builder.Run(
+                        async (context) => {
+                            await context.Response.WriteAsync("Hello, world!");
+                        }
+                    );
+                }
+            );
+        }
+    }
+}
