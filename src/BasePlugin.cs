da6c0ca996267202c84351a97d3eaadcc6bf7b6f
deximy <deximy@gmail.com>
2023-03-15 03:29:17 +0800

    feat: add meta information in the interface of plugins

--- src/BasePlugin.cs
+++ src/BasePlugin.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using System.Reflection;
 
 namespace Extensions.Middlewares.Plugin
 {
     public class BasePlugin : IPlugin
     {
+        public virtual string PluginName => Assembly.GetExecutingAssembly().GetName().Name ?? "Unknown Plugin";
+
+        public virtual string PluginVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown Version";
+
+        public virtual string? PluginDescription => null;
+
+        public virtual string Author => "Unknown Author";
+
+        public virtual string? ProjectUrl => null;
+
         public bool Enable(IApplicationBuilder app)
         {
             OnPluginStart(app);
--- src/IPlugin.cs
+++ src/IPlugin.cs
@@ -4,6 +4,16 @@ namespace Extensions.Middlewares.Plugin
 {
     public interface IPlugin
     {
+        public string PluginName { get; }
+
+        public string PluginVersion { get; }
+
+        public string? PluginDescription { get; }
+
+        public string Author { get; }
+
+        public string? ProjectUrl { get; }
+
         public bool Enable(IApplicationBuilder app);
 
         public bool Disable();
